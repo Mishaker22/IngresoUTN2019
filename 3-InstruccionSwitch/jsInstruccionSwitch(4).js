357b4862e79368b2370d2a5942994796c9778eba
Mishaker22 <47385993+Mishaker22@users.noreply.github.com>
2019-02-21 12:02:45 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,7 +1,25 @@
 function Mostrar()
 {
 //tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAno = document.getElementById('mes').value;
+
+switch(mesDelAno){
+    case"Enero":
+    case "Marzo":
+    case "Mayo":
+    case"Julio":
+    case "Agosto":
+    case"Octubre":
+    case "Diciembre":
+    alert ("tiene 31 dias");
+    break;
+    case "Febrero":
+    alert("tiene 28 dias");
+    break;
+    default:
+    alert("tiene 30 dias");
+    break;
+}
 
 //alert (mesDelAño);
 	
