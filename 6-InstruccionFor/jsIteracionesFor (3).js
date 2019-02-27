5c9055bfd2896755850036abc879927cebdb2a29
Mishaker22 <47385993+Mishaker22@users.noreply.github.com>
2019-02-27 12:30:04 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -3,6 +3,10 @@ function Mostrar()
 
 var repetciones = prompt("ingrese el número de repeticiones");
 
+for (var i=0; i<repetciones ;i++){
+    console.log("Hola UTRFRA");
+}
+
 
 
 }//FIN DE LA FUNCIÓN
